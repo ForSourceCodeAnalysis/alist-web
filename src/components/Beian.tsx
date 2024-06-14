fff1bc528992813ac59c2f61cf35bc2ee00cbfb9
ForSourceCodeAnalysis <sourceCodeAnalysis@aliyun.com>
2024-06-14 02:06:13 +0000

    optimize showing beian info

--- src/components/Beian.tsx
+++ src/components/Beian.tsx
@@ -2,13 +2,9 @@ import { Anchor, HStack, VStack } from "@hope-ui/solid"
 
 const Beian = () => {
   return (
-    <VStack class="footer" w="$full" py="$4">
-      <HStack spacing="$1">
-        <Anchor href="https://www.beian.gov.cn" external>
-          粤ICP备2021027476号-1
-        </Anchor>
-      </HStack>
-    </VStack>
+    <Anchor href="https://www.beian.gov.cn" external color="$neutral10">
+      粤ICP备2021027476号-1
+    </Anchor>
   )
 }
 export { Beian }
