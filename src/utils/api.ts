7f3e2418901b1a914fd68a91110d0579c3be09d4
varg1714 <51254954+varg1714@users.noreply.github.com>
2025-11-24 14:20:15 +0800

    fix(fs): Add skipExisting option to move and copy. (#252)
    * fix(fs): Add skipExisting option to move and copy.
    
    * fix(fs): Add merge option copy.
    
    * fix(fs): Code smell.
    
    fix(fs): Code smell.
    
    fix(fs): Code smell.
    
    * fix(fs): display the name of the tasks of the merge type.
    
    * fix: align Copy/Move modal actions with new skip options
    
    ---------
    
    Co-authored-by: jenfonro <799170122@qq.com>

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -91,8 +91,15 @@ export const fsMove = (
   dst_dir: string,
   names: string[],
   overwrite: boolean,
+  skip_existing: boolean,
 ): PEmptyResp => {
-  return r.post("/fs/move", { src_dir, dst_dir, names, overwrite })
+  return r.post("/fs/move", {
+    src_dir,
+    dst_dir,
+    names,
+    overwrite,
+    skip_existing,
+  })
 }
 
 export const fsRecursiveMove = (
@@ -108,8 +115,17 @@ export const fsCopy = (
   dst_dir: string,
   names: string[],
   overwrite: boolean,
+  skip_existing: boolean,
+  merge: boolean,
 ): PEmptyResp => {
-  return r.post("/fs/copy", { src_dir, dst_dir, names, overwrite })
+  return r.post("/fs/copy", {
+    src_dir,
+    dst_dir,
+    names,
+    overwrite,
+    skip_existing,
+    merge,
+  })
 }
 
 export const fsRemove = (dir: string, names: string[]): PEmptyResp => {
